58f0f93c415f2307d07002df27c0762018f9befd
Adrian A. Castrillon <joseadrian15@gmail.com>
2015-04-18 16:48:08 -0500

    Refactor: Se incluyen los modulos internos 'fs' y 'path'. El primero para poder manipular los archivos que se deben cargar cuando se hagan request, y el segundo para manejar de forma mas eficiente las rutas absolutas cuando se carguen en diferentes OS.

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-'use strict'
+'use strict' // Convención ECMA-Script6 Donde 'const' y 'let' reemplazan a 'var' para evitar problemas de 'Scope'
 
-const http = require('http')
-const port= process.env.PORT || 8080
+const http 	= 	require('http') // Peticion del modulo interno HTTP
+const fs 	=	require('fs') // Modulo interno File System para cargar archivos en las rutas
+const path 	=	require('path') // Modulo interno para el manejo de las rutas absolutas... contiene un variable __dirname que indica la carpeta actual de la aplicacion y funciona en todos los modulos
+const port 	= 	process.env.PORT || 8080 // Variable de Entorno para definir el puerto donde escucha el servidor
+const server = 	http.createServer() // Servidor Arranca y emite eventos "Event- Emmiter"
 
-const server = http.createServer() // Servidor Arranca y emite eventos "Event- Emmiter"
+	server.on('request',onRequest) // Event = 'request' y Emmit = ejecuta onRequest
+	server.on('listening', onListen) // Event = 'listening' y Emmit = ejecuta onListen
+	server.listen(port) //Metodo para que el servidor inicie la escucha por medio del puerto que se define en el parametro 'port'
 
-server.on('request',onRequest) // Event = 'request' y Emmit = ejecuta onRequest
-server.on('listening', onListen) // Event = 'listening' y Emmit = ejecuta onListen
 
-server.listen(port)
 
-//FUNCIONES 
+
+
+
+//FUNCIONES DEFINIDAS 
 
 function onRequest(req, res){
-	res.end('Hola Ubiquo')
+	let fileName = path.join(__dirname,'public','index.html')
+	fs.readFile(fileName, function(err , file){
+		if (err) {
+			return res.end(err.message )
+		};
+		res.end(file)
+	} )
 }
 
 function onListen(){
