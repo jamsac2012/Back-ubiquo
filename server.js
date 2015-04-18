15ca08192cc5a6ca1e967d4352ccf21edb616013
Adrian A. Castrillon <joseadrian15@gmail.com>
2015-04-18 14:14:50 -0500

    Refactor: Se inplementan las funciones de onRequest y onListen como respuesta 'Event-Emmit', para Darle orden al código

--- server.js
+++ server.js
@@ -3,6 +3,19 @@
 const http = require('http')
 const port= process.env.PORT || 8080
 
-const server = http.createServer()
+const server = http.createServer() // Servidor Arranca y emite eventos "Event- Emmiter"
+
+server.on('request',onRequest) // Event = 'request' y Emmit = ejecuta onRequest
+server.on('listening', onListen) // Event = 'listening' y Emmit = ejecuta onListen
 
 server.listen(port)
+
+//FUNCIONES 
+
+function onRequest(req, res){
+	res.end('Hola Ubiquo')
+}
+
+function onListen(){
+	console.log('Servidor activo en el puerto: ' + port)
+}
