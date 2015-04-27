d93e0660a9b57a51971217ad486ebda2593f6bc6
Adrian A. Castrillon <joseadrian15@gmail.com>
2015-04-27 18:01:43 -0500

    15 Refactor: Creacion de las funciones get/listar y post/guardar
    Se crean las rutas get y post como prueba de conexion con la BD ReQL.
    Con este refactor se pretende tener claro como funciona de forma basica
    la conexion con RethinkDB desde IO.js

--- router/index.js
+++ router/index.js
@@ -1,8 +1,15 @@
 const st = require('st') // Invocamos el modulo 'st'
 const path = require('path') // Invocamos el modulo 'path'
 const course = require ('course') //Invocamos modulo 'course'
+const r 	=	require('rethinkdb') // Peticion del modulo 'rethink'
+const config = 	require('../model/config.json'); // Peticion de modulo externo de configuracion para conexion con la BD ReQL.
 
-const router = course()
+
+
+// --------------------------------------------------------------------------------------------
+//		Definicion de ruta estatica para encontrar todos los archivos de la carpeta ./public
+// --------------------------------------------------------------------------------------------
+
 const mount = st({  // Constante que me carga la funcionalidad de st que requiere dos propiedades
 	path: path.join( __dirname, '..', 'public'), // metodo .join para armar la ruta estatica en la carpeta public y todos sus archivos
 	index: 'index.html',  // Me pregunta cual sera el archivo para indexar por defecto. En este caso abrirá index.html
@@ -10,18 +17,9 @@ const mount = st({  // Constante que me carga la funcionalidad de st que requier
 })
 
 
-// RUTAS DEFINIDAS SEGUN EL METODO
-
-router.post('/guardar', function(req, res){ // Ruta para guardar los RegisterId de los dispositivos
-
-})
-
-router.get('/validar', function(req, res){ // Ruta para validar que las credenciales existen en Ubiquo.
-
-})
-
-
-//FUNCIONES DE RESPUESTA
+// ----------------------------------------------------------------------------------------
+//		Funcion que será exportada en el modulo - Responde a la ruta estatica ./public 
+// ----------------------------------------------------------------------------------------
 
 function onRequest(req, res){ 
 	mount(req,res, function(err){  // Llamo al montaje de la ruta statica para saber si esta disponible en la respuesta y adhiero un callback por si es un error
@@ -37,10 +35,66 @@ function onRequest(req, res){
 	})	
 }
 
+
+// ------------------------------------------------------------------------------
+//		Definicion de Funciones para el manejo de los datos en la API-REST
+// ------------------------------------------------------------------------------
+
+function listar(req, res) {
+	console.log('_____________________');
+	console.log('API - ubiquo/usuarios');
+	
+	r.connect(config.rethinkdb, function(err, conn){
+		if (err) throw err
+
+		r.table('usuarios').run(conn, function(err, cursor) {
+		    if (err) throw err;
+		    cursor.toArray(function(err, result) {
+		        if (err) throw err;
+		        console.log(JSON.stringify(result, null, 2));
+		    });
+		});
+	})
+}
+
+function guardar(req, res){
+	r.connect(config.rethinkdb, function(err, conn){
+		if (err) throw err
+
+		r.table('usuarios').insert({
+			usuario:'Amparo',
+			pass:'1234',
+			regId:'qwerdssdfgkjhg'
+			}).run(conn, function(err, result) {
+			    if (err) throw err;
+			    console.log(JSON.stringify(result, null, 2));
+		});
+	})
+}
+
+
+
+// ------------------------------------------
+//		Funcion para manejor error 500 
+// ------------------------------------------
+
+
 function fail (err, res){  // Funcion para definir o encausar un error cuando no sabemos que paso
 	res.statusCode = 500 	// Asigna un codigo de respuesta al error
 	res.setHeader('Content-Type', 'text/plain')  // Prepara el browser para recibir un texto plano
 	res.end(err.message)  //  Sirve el error en el browser.
 }
 
+
+
+// ------------------------------------------
+//		Manejo de Rutas para la API
+// ------------------------------------------
+
+const router = course()
+
+router.get('/mostrar', listar)  // Ruta para encontrar el metodo de Listar todos los datos de la BD.
+router.post('/guardar', guardar) // Ruta para encontrar el metodo de guardar datos en la BD.
+
 module.exports = onRequest   //  Exporta la funcion onRequest para ser llamada a nivel global en la aplicacion.
+
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 'use strict' // Convención ECMA-Script6 Donde 'const' y 'let' reemplazan a 'var' para evitar problemas de 'Scope'
 
-const r 	=	require('rethinkdb') // Peticion del modulo 'rethink'
-const config = 	require('./model/config.json'); // Peticion de modulo externo de configuracion para conexion con la BD ReQL.
 const http 	= 	require('http') // Peticion del modulo interno HTTP
-const fs 	=	require('fs') // Modulo interno File System para cargar archivos en las rutas
 const path 	=	require('path') // Modulo interno para el manejo de las rutas absolutas... contiene un variable __dirname que indica la carpeta actual de la aplicacion y funciona en todos los modulos
 const router =	require('./router')  // Se invoca el metodo o funcion onRequest que se encuentra modularizado en un archivo con la logica de enrutamiento.
 const port 	= 	process.env.PORT || 8081 // Variable de Entorno para definir el puerto donde escucha el servidor
@@ -14,18 +11,7 @@ const server = 	http.createServer() // Servidor Arranca y emite eventos "Event- 
 	server.on('request',router) // Event = 'request' y Emmit = ejecuta onRequest dentro del modulo "./router"
 	server.on('listening', onListen) // Event = 'listening' y Emmit = ejecuta onListen
 	server.listen(port) //Metodo para que el servidor inicie la escucha por medio del puerto que se define en el parametro 'port'
-
-// CONEXION CON LA BD
-
-	r.connect(config.rethinkdb)
-		.then(function(conn) {
-		    console.log(conn)
-		    console.log('Conexion Establecida')
-			})
-		.error(function(error){
-		    console.log(error.message);
-			});
-
+	
 
 //FUNCIONES DEFINIDAS 
 
